c57c96f01e9737086b0a8a542cffbd08f6d0feb4
tiiedye <tiiedye@gmail.com>
2021-11-26 10:32:14 -0800

    wrote program using dictionaries to store bidders and amount of bid, then determined highest bid amount

--- main.py
+++ main.py
@@ -1,2 +1,44 @@
 # Blind Auction
+from art import logo
+import os
 
+
+# Function to clear console
+def clear():
+    wipe = '\n' * 100
+    print(wipe)
+
+
+bids = {}
+loop = True
+
+while loop:
+    print(logo)
+    name = input("What is the bidder's name?: ")
+    bid_amount = input("How much would you like to bid?: $")
+
+    bids[name] = bid_amount
+
+    add_bidder = input("Is there another bidder? Y/N: ").lower()
+
+    if add_bidder == 'y':
+        clear()
+    elif add_bidder == 'n':
+        loop = False
+        clear()
+    else:
+        clear()
+        loop = False
+        print("Invalid command, bidding will end")
+
+highest_bidder = ""
+highest_bid = 0
+for bidder in bids:
+    bid = bids[bidder]
+    bid = float(bid)
+    if bid > highest_bid:
+        highest_bidder = bidder
+        highest_bid = bid
+
+print(logo)
+print(f"The winner of the auction is {highest_bidder}, with ${bids[highest_bidder]}")
